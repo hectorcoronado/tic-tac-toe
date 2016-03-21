4092f8f7c6a6486acd9e6d8146a914419fc98681
hectorcoronado <coronado.hector@gmail.com>
2016-03-21 00:12:36 -0700

    Work in progress, with much left to be done

--- js/app.js
+++ js/app.js
@@ -1,6 +1,31 @@
 // wait for the DOM to finish loading
 $(document).ready(function() {
-  // all code to manipulate the DOM
-  // goes inside this function
+// all code to manipulate the DOM
+// goes inside this function
+
+var clickCounter = 0; //This variable will keep track of player turn
+var gameBoard = []; //
+
+$('.box').on('click', function handleClick(event) {
+   if ( $(this).length === 0) {
+    	if ( clickCounter % 2 === 0) {
+       		$(this).text('X');
+       		clickCounter++;
+    	} else if ( clickCounter % 2 !== 0) {
+        		$(this).text('O');
+        		clickCounter++;
+    		}
+   } 
+});
+
 
 });
+
+/* This will be incorporated to the above function to theoretically
+automatically result in a drawn game.
+
+else if (clickCounter > 8) {
+   	alert("Game drawn!");
+   } 
+
+*/
