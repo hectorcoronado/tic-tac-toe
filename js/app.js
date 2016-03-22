e5caae085d1b37ef59e474ba98ffbdaf7b4d5760
hectorcoronado <coronado.hector@gmail.com>
2016-03-21 22:50:45 -0700

    Got game to work! Reset button & styling to come.

--- js/app.js
+++ js/app.js
@@ -5,42 +5,58 @@ $(document).ready(function() {
 
 var clickCounter = 0; //This variable will keep track of player turn
 var gameBoard = ['.','.','.','.','.','.','.','.','.'];
- //Index pos=     0   1   2   3   4   5   6   7   8 (9 possible positions)
- //This array willkeep track of X's and O's to help determine winner
-var gameBoardInd; //Will be an integer
+ //Index/id       0   1   2   3   4   5   6   7   8
+ //This array will track X's and O's to determine winner
 
 $('.box').on('click', function handleClick(event) {
    if ( $(this).text() === '') {
     	if ( clickCounter % 2 === 0) {
        		$(this).text('X');
-          //The parseInt() function parses a string argument and
-          //returns an integer of the specified radix (the base in mathematical numeral systems).
-          var cellX = parseInt( $(this).attr('id') );
-          gameBoard[cellX] = 'X';
-          //add X's to id's (which equal index) of gameBoard array
-          //$(this).(gameBoard)
+          var cellX = parseInt( $(this).attr('id') ); //parseInt method turns 'X' string added to .box to index/id #
+          gameBoard[cellX] = 'X'; //so that 'X's can be assigned to gameBoard at index/id #
           console.log(gameBoard); //Keep track of current board state
        		clickCounter++;
     	} else if ( clickCounter % 2 !== 0) {
-        console.log($(this).attr('id'));
         		$(this).text('O');
             var cellY = parseInt( $(this).attr('id') );
-            gameBoard[cellY] = 'O';
-            //add O's to id's (which equal index) of gameBoard array
+            gameBoard[cellY] = 'O'; //as above (line 16), so that 'Y' can be assigned to gameBoard at index/id number
             console.log(gameBoard); //Keep track of current board state
         		clickCounter++;
     		}
    }
 });
+$('.box').on('click', function checkWinner(event) {
+    if (
+          ( (gameBoard[0]==='X' && gameBoard[1] === 'X') && (gameBoard[2] === 'X') ) ||
+          ( (gameBoard[3]==='X' && gameBoard[4] === 'X') && (gameBoard[5] === 'X') ) ||
+          ( (gameBoard[6]==='X' && gameBoard[7] === 'X') && (gameBoard[8] === 'X') ) ||
+          ( (gameBoard[0]==='X' && gameBoard[3] === 'X') && (gameBoard[6] === 'X') ) ||
+          ( (gameBoard[1]==='X' && gameBoard[4] === 'X') && (gameBoard[7] === 'X') ) ||
+          ( (gameBoard[2]==='X' && gameBoard[5] === 'X') && (gameBoard[8] === 'X') ) ||
+          ( (gameBoard[0]==='X' && gameBoard[4] === 'X') && (gameBoard[8] === 'X') ) ||
+          ( (gameBoard[6]==='X' && gameBoard[4] === 'X') && (gameBoard[2] === 'X') )
 
+        ) {
+
+            alert("Player X wins!");
+
+    } else if (
+
+            ( (gameBoard[0]==='O' && gameBoard[1] === 'O') && (gameBoard[2] === 'O') ) ||
+            ( (gameBoard[3]==='O' && gameBoard[4] === 'O') && (gameBoard[5] === 'O') ) ||
+            ( (gameBoard[6]==='O' && gameBoard[7] === 'O') && (gameBoard[8] === 'O') ) ||
+            ( (gameBoard[0]==='O' && gameBoard[3] === 'O') && (gameBoard[6] === 'O') ) ||
+            ( (gameBoard[1]==='O' && gameBoard[4] === 'O') && (gameBoard[7] === 'O') ) ||
+            ( (gameBoard[2]==='O' && gameBoard[5] === 'O') && (gameBoard[8] === 'O') ) ||
+            ( (gameBoard[0]==='O' && gameBoard[4] === 'O') && (gameBoard[8] === 'O') ) ||
+            ( (gameBoard[6]==='O' && gameBoard[4] === 'O') && (gameBoard[2] === 'O') )
+
+      ) {
+              alert("Player O wins!");
+
+            } else if (clickCounter > 8) {
+              alert("Draw!");
+            }
+  });
 
 });
-
-/* This will be incorporated to the above function to theoretically
-automatically result in a drawn game.
-
-else if (clickCounter > 8) {
-   	alert("Game drawn!");
-   }
-
-*/
